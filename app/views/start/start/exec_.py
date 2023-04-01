1e2afa7ff3abb3969d4a7cf06f9ee42079a244c5
Romulo Pauliv <romulopauliv@bk.ru>
2023-04-01 18:10:02 -0300

    added in execution method

--- app/views/start/start/exec_.py
+++ app/views/start/start/exec_.py
@@ -17,9 +17,13 @@ class StartChatExec(object):
     def __init__(self) -> None:
         self.init_command: str = COMMANDS_LIST["start"]
         self.start_chat = StartChat()
+        self.in_execution: list[str] = []
     
     def exec_(self, message: dict[str, Any]) -> None:
         received_message: str = message["text"]
-        
-        if received_message == self.init_command:
+        chat_id: str = message["chat_id"]
+
+        if received_message == self.init_command and chat_id not in self.in_execution:
+            self.in_execution.append(chat_id)
             self.start_chat.start_response(message)
+            self.in_execution.remove(chat_id)
