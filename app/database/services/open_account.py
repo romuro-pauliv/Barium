ae2ca0b367dc4db7c9229fdd5d955ce0e34e3c37
Romulo Pauliv <romulopauliv@bk.ru>
2023-04-25 21:00:50 -0300

    added database operation identificator

--- app/database/services/open_account.py
+++ app/database/services/open_account.py
@@ -28,14 +28,17 @@ class MongoOpenAccount(object):
         database_name: str = f"AYLA_{chat_id}"
         OpenAccountLog.open_database(chat_id, database_name)
         
+        # Database Operation |-----------------------------------------------------------------------------------------|
         self.mongo[database_name]["/LOG"].insert_one(self.log(chat_id, username, "open account"))
         LogDBLog.show(chat_id, database_name, "/LOG", "open account")
         self.mongo.AYLA_LOG.MAINLOG.insert_one(self.log(chat_id, username, "open account"))
         LogDBLog.show(chat_id, "AYLA_LOG", "MAINLOG", "open account")
+        # |------------------------------------------------------------------------------------------------------------|
         
     def open_wallet_list(self, chat_id: str, username: str, wallet_name: str, amount: str, wallet_obs: str) -> None:
         database_name: str = f"AYLA_{chat_id}"
         
+        # Database Operation |-----------------------------------------------------------------------------------------|
         self.mongo[database_name]["/WALLETS"].insert_one(
             {
                 "datetime": datetime.datetime.utcnow(),
@@ -50,10 +53,12 @@ class MongoOpenAccount(object):
         LogDBLog.show(chat_id, database_name, "/LOG", "open wallet list")
         self.mongo.AYLA_LOG.MAINLOG.insert_one(self.log(chat_id, username, "open wallet list"))
         LogDBLog.show(chat_id, "AYLA_LOG", "MAINLOG", "open wallet list")
+        # |------------------------------------------------------------------------------------------------------------|
     
     def open_wallet_collection(self, chat_id: str, username: str, wallet_name: str) -> None:
         database_name: str = f"AYLA_{chat_id}"
         
+        # | Database Operation |---------------------------------------------------------------------------------------|
         self.mongo[database_name][wallet_name].insert_one(
             {"datetime": datetime.datetime.utcnow(), "log": f"hello, {wallet_name}"}
         )
@@ -63,6 +68,7 @@ class MongoOpenAccount(object):
         LogDBLog.show(chat_id, database_name, "/LOG", f"open wallet {wallet_name}")
         self.mongo.AYLA_LOG.MAINLOG.insert_one(self.log(chat_id, username, f"open wallet {wallet_name}"))
         LogDBLog.show(chat_id, "AYLA_LOG", "MAINLOG", f"open wallet {wallet_name}")
+        # |------------------------------------------------------------------------------------------------------------|
     
     def init_account(self, message: dict[str, str], in_cache: dict[str, Union[str, float]]) -> None:
         chat_id: str = message["chat_id"]
