b24d59312d52672486a35ec97596883aec411abc
Romulo Pauliv <romulopauliv@bk.ru>
2023-05-12 14:51:32 -0300

    refactor the Start Driver MS

--- microsservices/Start/api/error/send_to_telegram.py
+++ microsservices/Start/api/error/send_to_telegram.py
@@ -11,4 +11,10 @@ from api.api.api_request import TelegramApiRequest
 # |--------------------------------------------------------------------------------------------------------------------|
 
 def system_down_message(chat_id: str) -> None:
+    """
+    Send a message to the telegram Ayla user that the system is down.
+
+    Args:
+        chat_id (str): chat id from the message that Telegram API generated
+    """
     TelegramApiRequest().send_message(chat_id, ErrorMessages.ERROR_MESSAGE["message"])
--- microsservices/Start/api/services/command_recognizer.py
+++ microsservices/Start/api/services/command_recognizer.py
@@ -19,27 +19,46 @@ import requests
 
 class Driver(object):
     def __init__(self) -> None:
+        """
+        Loading sub microservices data routes
+        """
         self.sub_services_conn: dict[str, dict] = MicrosservicesAPI.MS_ROUTES["sub_services"]
         self.commands: dict[str, str] = MicrosservicesAPI.COMMANDS
         
         self.sub_services_conn_name: list[str] = [i for i in self.commands.keys()]
     
-    def log_thread(self, func: Callable[..., None], args_: tuple[Any]) -> None:
-        threading.Thread(target=func, args=args_).start()
+    def log_thread(self, args_: tuple[Any]) -> None:
+        """
+        Execute a SendToLog().report in another thread
+        Args:_
+            args_ (tuple[Any]): Arguments of the function SendToLog().report
+        """
+        threading.Thread(target=SendToLog().report, args=args_).start()
+    
+    def log_report(self, HOST: str, PORT: str, log_data: str, chat_id: str) -> None:
+        """
+        Resume the building of the Log report
+        Args:
+            HOST (str): Inform the HOST executed
+            PORT (str): Inform the PORT executed
+            log_data (str): Lower key content log_report data
+            chat_id (str): chat_id from Telegram client message
+        """
+        log_schema: list[str] = LogSchema.LOG_REPORT_MSG["connections"][log_data]
+        log_text: str = f"[{HOST}:{PORT}]"
+        args_ = tuple[str] = (log_schema[0] + log_text, log_schema[1], chat_id)
+        self.log_thread(args_)
     
     def completed_connection_log(self, HOST: str, PORT: str, chat_id: str) -> None:
-        log_schema: list[str] = LogSchema.LOG_REPORT_MSG["connections"]["ms_message_response"]
-        log_text: str = f"[{HOST}:{PORT}]"
-        args_: tuple[str] = (log_schema[0] + log_text, log_schema[1], chat_id)
-        self.log_thread(SendToLog().report, args_)
+        self.log_report(HOST, PORT, "ms_message_response", chat_id)
     
     def failed_connection_log(self, HOST: str, PORT: str, chat_id: str) -> None:
-        log_schema: list[str] = LogSchema.LOG_REPORT_MSG["connections"]["failed_ms_message_response"]
-        log_text: str = f"[{HOST}:{PORT}]"
-        args_: tuple[str] = (log_schema[0] + log_text, log_schema[1], chat_id)
-        self.log_thread(SendToLog().report, args_)
+        self.log_report(HOST, PORT, "failed_ms_message_response", chat_id)
         
     def direct_to(self, message: dict[str, str | list]) -> None:
+        """
+        Direct the message data to specific microservice based on the given command in the message
+        """
         for service_name in self.sub_services_conn_name:
             if message["text"] == self.commands[service_name]:
                 HOST: str = self.sub_services_conn[service_name]["HOST"]
