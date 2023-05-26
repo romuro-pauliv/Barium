d376c1733420f5419dbe2e9c6fc6953547924ab7
Romulo Pauliv <romulopauliv@bk.ru>
2023-05-26 15:26:22 -0300

    modified the connection instance in HELP command

--- microservices/Start/commands/null/api/services/respond_client.py
+++ microservices/Start/commands/null/api/services/respond_client.py
@@ -8,11 +8,11 @@
 # | Imports |----------------------------------------------------------------------------------------------------------|
 from api.services.tools.random_msg_from_list import random_msg_from_list
 from api.config.paths import Messages, LogSchema, MicrosservicesAPI
+
+from api.connections.sender import connect_with_sender
 from api.connections.send_log import SendToLog
-from api.errors.send_to_telegram import system_down_message
 
 import threading
-import requests
 # |--------------------------------------------------------------------------------------------------------------------|
 
 
@@ -56,12 +56,12 @@ class RespondClient(object):
             "message": f"{random_msg[0]}{message['username']}{random_msg[1]}",
             "microservice": [self.whoami["NAME"], str(self.whoami["HOST"] + ":" + self.whoami["PORT"])]
         }
-        
-        try:
-            requests.post(f"{self.HOST}:{self.PORT}{self.PATH}{self.ENDPOINT}", json=msg_to_send)
-            self.log_report("connections", "sent_completed", message["chat_id"])
-        except requests.exceptions.ConnectionError:
-            self.log_report("connections", "sent_failed", message["chat_id"])
-            system_down_message(message["chat_id"])
-            self.log_report("telegram_api", "error_message", message["chat_id"])
-        
+        connect_with_sender(
+                route={
+                    "HOST":self.HOST, "PORT":self.PORT,
+                    "PATH":self.PATH, "ENDPOINT":self.ENDPOINT
+                },
+                post_json=msg_to_send,
+                chat_id=message["chat_id"],
+                log_report_function=self.log_report
+            )
